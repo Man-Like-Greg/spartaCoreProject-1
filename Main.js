79da7dbedc1e36234abb3808f18669451b40a686
Tech-A44 <gmafutala07@gmail.com>
2017-08-17 15:55:35 +0100

    Sprint 3 completed

--- Main.js
+++ Main.js
@@ -3,9 +3,6 @@ $(function () {
 	'images/Thierry-Henry.jpg','images/lionel-messi.jpg','images/david.jpg',
 	'images/ronaldo.jpg','images/david.jpg','images/ronaldo.jpg','images/Minionspic.jpeg'];
 
-	var arrayOfImages2 = ['images/Thierry-Henry.jpg','images/lionel-messi.jpg',
-	'images/Thierry-Henry.jpg','images/lionel-messi.jpg','images/david.jpg',
-	'images/ronaldo.jpg','images/david.jpg','images/ronaldo.jpg','images/Minionspic.jpeg'];
 	// find reset
 	var $reset = $('#reset')
 	// find the memory board
@@ -20,26 +17,23 @@ $(function () {
 	var $selected = $('.selected');
 	// find the first game selection
 	var $startGame1 = $('#startGame1');
-	// find the second game selection
-	var $startGame2 = $('#startGame2');
-	// find the third game selection
-	var $startGame3 = $('#startGame3');
 	// find the introduction 
 	var $scenario = $('#scenario');
 	// find paired
 	var $paired = $('#paired');
 	// find leader board
-	var $leaderBoard = $('#leaderBoard')
+	var $leaderBoard = $('#leaderBoard');
+	// find first name
+	var $firstname = $('#firstname')
+
 	var counter = 0;
 	var pairs = 0;
-	// getting the seconds
-	var seconds = 10;
+	var seconds = 60;
 	var inteval;
 
 	$reset.hide();
 	$scores.hide();
 	$memoryBoard.hide();
-
 
 	function setUpEventListeners() {
 		// start the game
@@ -56,41 +50,7 @@ $(function () {
 			createNewBoard();
 		});
 
-		$startGame2.click(function(event) {
-		  counter = 0;
-		  $counter.html(counter);
-			$scenario.slideUp();
-			$scores.show();
-			$reset.show();
-			$memoryBoard.show();
-
-			$('#grids').html("");
-			startTimer();
-			createNewBoard();
-		});
-
-		$startGame3.click(function(event) {
-		  counter = 0;
-		  $counter.html(counter);
-			$scenario.slideUp();
-			$scores.show();
-			$reset.show();
-			$memoryBoard.show();
-
-			$('#grids').html("");
-			startTimer();
-			createNewBoard();
-		});
-
-		$leaderBoard.click(function(event) {
-			$scenario.slideUp();
-			$reset.show();
-			$memoryBoard.show();
-			$('#grids').html("");
-			leadBoard();
-		})
-
-	// if statement to allow matching the grids
+		// if statement to allow matching the grids
 		$('#grids').on('click', 'div', function(event) {
 			if (!$(this).hasClass('match')) {
 				$(this).addClass('selected');
@@ -109,7 +69,7 @@ $(function () {
 	  	// clear the inteval
 	  	clearInterval(inteval);
 	  	//set the seconds back to original
-	  	seconds = 50;
+	  	seconds = 60;
   		loser();
 	  }  
 	}
@@ -125,10 +85,12 @@ $(function () {
 		while (0 !== currentIndex) {
   		randomIndex = Math.floor(Math.random() * currentIndex);
   		currentIndex -= 1;
-
-	    tempValue = array[currentIndex]; // take the current index and adds it to an array
-	    array[currentIndex] = array[randomIndex]; //then takes the current index array and makes it the random index
-	    array[randomIndex] = tempValue; // which then is added back into the temporary value
+  		// take the current index and adds it to an array
+	    tempValue = array[currentIndex]; 
+	    //then takes the current index array and makes it the random index
+	    array[currentIndex] = array[randomIndex]; 
+	    // which then is added back into the temporary value
+	    array[randomIndex] = tempValue; 
 		}
 		return array;
 	} 
@@ -143,30 +105,38 @@ $(function () {
 
 		$('div img').hide();
   } 
-  	// show the box
+  // show the boxes
 	function displayBox (){
 		var $selected = $('.selected');
 		var $match = $('.match');
+		var paired = new Audio("audio/paired.mp3");
 
 		counter = counter + 1;
     $counter.html(counter);
+
+    var src3 = $selected.eq(1).find('img').attr('src');
+    var src4 = $selected.eq(0).find('img').attr('src');
+
+		if (src3 === "images/Minionspic.jpeg") {
+			joker();
+		}
+		else if (src4 === "images/Minionspic.jpeg") {
+			joker();
+		}
 
 		if($selected.length === 2) {
 
 			var src1 = $selected.eq(1).find('img').attr('src');
 			var src2 = $selected.eq(0).find('img').attr('src');
 
-			// if (src1 || src2 = Minionspic.jpeg) {
-			// 	loser();
-			// }
-
 			if (src1 === src2) {
+				paired.play();
 				$selected.removeClass('selected');
 				$selected.addClass('match');
 				pairs = pairs + 1;
 				$paired.html("Matched: " +pairs);
 
-				if($('.match').length === 12) {
+				if($('.match').length === 8) {
 					winner();
 				}
 
@@ -179,50 +149,67 @@ $(function () {
 			}  		
   	}
   }
-  // leader board function
-  function leadBoard() {
-  	// var $newDiv= $("#newDiv");
-	  // $newDiv.className="save";
-	  // $newDiv.html=text;
-   //  document.body.appendChild(newDiv);
-
-  }
-
 
   // when the timer runs out function
   function loser() {
+  	var lose = new Audio("audio/lose.mp3");
+		lose.play();
 
   	setTimeout(function() {
 			var $scores = $('#scores');
 			$scores.fadeOut();
-  		$('#grids div').fadeOut();			
+  		$('#grids div').fadeOut();
+  		$memoryBoard.fadeOut();			
   	}, 500);
   	
   	setTimeout(function () {
 			var loseMessage = 
 			'<p class="losingMessage"><strong>GAME OVER!</strong></p><p>Time ran out.</p>' //this is to add a message in html
 			$('#scenario').fadeIn();
-			$('#startGame1').html('<a class="link" href="index.html">Play again?</a>'); // change the button on html
+			$('#startGame1').html('<a class="link" href="index.html">Back</a>'); // change the button on html
 			$('.rules').html(loseMessage).addClass('losingMessage');
 		}, 1000);
   }  
+  // when the joker gets selected
+  function joker() {
+  	var lose = new Audio("audio/lose.mp3");
+		lose.play();
+
+  	setTimeout(function() {
+			var $scores = $('#scores');
+			$scores.fadeOut();
+  		$('#grids div').fadeOut();
+  		$memoryBoard.fadeOut();			
+  	}, 500);
+  	
+  	setTimeout(function () {
+			var jokeMessage = 
+			'<p class="jokingingMessage"><strong>GAME OVER!</strong></p><p>HAHAHAHAHA the Minions have found you.</p>' //this is to add a message in html
+			$('#scenario').fadeIn();
+			$('#startGame1').html('<a class="link" href="index.html">Back</a>'); // change the button on html
+			$('.rules').html(jokeMessage).addClass('jokingMessage');
+		}, 1000);
+  }
 
   // when the user wins   
 	function winner() {
+		var applause = new Audio("audio/small_indoor_crowd_applause.mp3");
+		applause.play();
 		var $theTimer = $('#timer');
 		$theTimer.off();
 
 		setTimeout(function() {
 			var $scores = $('#scores');
 			$scores.fadeOut();
-  		$('#grids div').fadeOut();			
+  		$('#grids div').fadeOut();
+  		$memoryBoard.fadeOut();			
   	}, 1000);
   	
   	setTimeout(function () {
 			var winMessage = 
-			'<p class="winningMessage"><strong>Congratulations!</strong></p><p>You completed this in ' + counter + ' clicks.</p>' //this is to add a message in html
+			'<p class="winningMessage"><strong>Congratulations!</strong></p><p>You completed this in ' + counter + ' clicks.</p>'; 
 			$('#scenario').fadeIn();
-			$('#startGame1').html('<a class="link" href="index.html">Play again?</a>');
+			$('#startGame1').html('<a class="link" href="index.html">Back</a>');
 			$('.rules').html(winMessage).addClass('winningMessage');
 		}, 2000);		
 	}	
